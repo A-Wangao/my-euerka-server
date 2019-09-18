b5f607db5966ec436e39b61a6f2a59af1e02b41a
wangao <2451516728@qq.com>
2019-09-18 10:13:15 +0800

    no message

--- my-euerka-server/src/main/java/com/zb/controller/HomeController.java
+++ my-euerka-server/src/main/java/com/zb/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.zb.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String index(){
         return "hello";
     }
